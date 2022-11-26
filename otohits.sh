5c764b2ec4949280e1a51388697721e249470d68
koRajesh <118733471+koRajesh@users.noreply.github.com>
2022-11-26 13:15:58 +0530

    Update otohits.sh

--- otohits.sh
+++ otohits.sh
@@ -1,6 +1,6 @@
 wget https://www.otohits.net/dl/OtohitsApp_5068_linux_portable.tar.gz > /dev/null
 mkdir OtohitsApp
 cd OtohitsApp
-echo "/login:3c1d0624-b07f-4009-bfc0-520a52e0a76e" > otohits.ini
+echo "/login:20a3792a-11f7-41d0-8eb9-39e9bb48975d" > otohits.ini
 tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
 ./otohits-app
